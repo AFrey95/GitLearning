6f8f1fae4ca7ab9f18ce26c9d16148efabb64379
AFrey95 <andyfrey95@gmail.com>
2017-05-31 14:10:17 -0400

    Created main method

--- src/com/Main.java
+++ src/com/Main.java
@@ -1,5 +1,17 @@
 package com;
 
 public class Main {
-
+	public static void main(String[] args) {
+		Adderator adderator = new Adderator();
+		String equation = "8 plus three";
+		int result = 0;
+		
+		try {
+			result = adderator.doAdd(equation);
+		} catch (Exception e) {
+			System.out.println("Couldn't compute '" + equation + "'");
+		}
+		
+		System.out.println(equation + " = " + result);
+	}
 }
